a22feaea5ba150ed45413bbe887f40fb842950fd
LisDemlord <lis.demlord@gmail.com>
2023-12-17 14:33:30 +0500

    Added shakerSort fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,14 +16,58 @@ fun bubbleSort(arr: IntArray) {
     }
 }
 
+fun shakerSort(arr: IntArray) {
+    val n = arr.size
+    var swapped: Boolean
+    var start = 0
+    var end = n - 1
+
+    do {
+        swapped = false
+
+        // Проход слева направо
+        for (i in start until end) {
+            if (arr[i] > arr[i + 1]) {
+                // меняем элементы местами, если текущий больше следующего
+                val temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                swapped = true
+            }
+        }
+
+        if (!swapped) break
+
+        swapped = false
+        end--
+
+        // Проход справа налево
+        for (i in end - 1 downTo start) {
+            if (arr[i] > arr[i + 1]) {
+                // меняем элементы местами, если текущий больше следующего
+                val temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                swapped = true
+            }
+        }
+
+        start++
+    } while (swapped)
+}
+
 fun main() {
     // Генерируем массив из 100 случайных чисел
-    val arrayToSort = IntArray(50) { Random.nextInt(1, 1000) }
+    val arrayToSort = IntArray(10) { Random.nextInt(1, 1000) }
 
     println("Исходный массив: ${arrayToSort.joinToString()}")
 
-    // Вызываем функцию сортировки
-    bubbleSort(arrayToSort)
+    // Создаем копию массива и сортируем ее пузырьковой сортировкой
+    val arrayCopy = arrayToSort.clone()
+    bubbleSort(arrayCopy)
+    println("Отсортированный массив (пузырьковая сортировка): ${arrayCopy.joinToString()}")
 
-    println("Отсортированный массив: ${arrayToSort.joinToString()}")
+    // Сортируем исходный массив шейкерной сортировкой
+    shakerSort(arrayToSort)
+    println("Отсортированный массив (шейкерная сортировка): ${arrayToSort.joinToString()}")
 }
